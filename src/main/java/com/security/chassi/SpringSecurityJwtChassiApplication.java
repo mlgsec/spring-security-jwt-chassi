b2271c617aaae7523b9d02fdfa2e0fa170fa3bb7
mlgsec <sec.mateuslgomes@gmail.com>
2025-04-05 19:44:05 -0300

    Start project

---
+++ src/main/java/com/security/chassi/SpringSecurityJwtChassiApplication.java
@@ -0,0 +1,13 @@
+package com.security.chassi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtChassiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtChassiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/chassi/SpringSecurityJwtChassiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.chassi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtChassiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
