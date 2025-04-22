0f537a8e48cdf39458af54da18f39e1890fac53b
mlgsec <sec.mateuslgomes@gmail.com>
2025-04-22 19:42:05 -0300

    feat: add User entity with many-to-many relationship with Role

--- src/main/java/com/security/chassi/user/Role.java
+++ src/main/java/com/security/chassi/user/Role.java
@@ -1,4 +1,24 @@
 package com.security.chassi.user;
 
-public enum Role {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Data
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
 }
