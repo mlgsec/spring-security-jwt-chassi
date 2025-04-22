8b99273951dc9f554d80add7fd6c955cad8bfe1f
mlgsec <sec.mateuslgomes@gmail.com>
2025-04-22 19:41:15 -0300

    feat: update User entity to use Long for id and implement ManyToMany relationship with Role

--- src/main/java/com/security/chassi/user/User.java
+++ src/main/java/com/security/chassi/user/User.java
@@ -1,70 +1,26 @@
-package com.security.chassi.user;
+    package com.security.chassi.user;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+    import jakarta.persistence.*;
+    import lombok.Data;
 
-import java.util.Collection;
-import java.util.List;
+    import java.util.HashSet;
+    import java.util.Set;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "users")
-public class User implements UserDetails {
+    @Entity
+    @Data
+    @Table(name = "users")
+    public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
-    private String  firstName;
-    private String lastName;
-    private String email;
-    private String password;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String email;
+        private String password;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
+        @ManyToMany(fetch = FetchType.EAGER)
+        @JoinTable(name = "user_roles",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+        private Set<Role> roles = new HashSet<>();
 
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
     }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-}
