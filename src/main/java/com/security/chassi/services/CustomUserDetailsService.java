e89895a47186a0353ede838071c2a2263ae91244
mlgsec <sec.mateuslgomes@gmail.com>
2025-04-22 19:25:29 -0300

    feat: implement custom user details service for loading user by email

---
+++ src/main/java/com/security/chassi/services/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.security.chassi.services;
+
+import com.security.chassi.user.User;
+import com.security.chassi.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), user.getRoles());
+    }
+}
