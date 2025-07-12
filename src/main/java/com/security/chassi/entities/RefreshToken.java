a44753acbbed5ed8c5fe8767cfbc0763a348d2a0
mateus <sec.mateuslgomes@gmail.com>
2025-07-12 13:10:06 -0300

    feat: Create refresh_tokens table and update RefreshToken entity with constraints

--- src/main/java/com/security/chassi/entities/RefreshToken.java
+++ src/main/java/com/security/chassi/entities/RefreshToken.java
@@ -7,17 +7,21 @@ import java.util.Date;
 
 @Entity
 @Data
+@Table(name = "refresh_tokens")
 public class RefreshToken {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 255, nullable = false, unique = true)
     private String token;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "expiry_date", nullable = false)
     private Date expiryDate;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
