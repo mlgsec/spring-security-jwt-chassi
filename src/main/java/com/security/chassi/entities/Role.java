66b3e01e83801841dbbd41c2aa288bf6f1432333
mateus <sec.mateuslgomes@gmail.com>
2025-07-12 13:02:33 -0300

    feat: Create roles table and update Role entity for consistency

--- src/main/java/com/security/chassi/entities/Role.java
+++ src/main/java/com/security/chassi/entities/Role.java
@@ -1,20 +1,19 @@
 package com.security.chassi.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Data
+@Table(name = "roles")
 public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 100, nullable = false, unique = true)
     private String name;
 
     @Override
